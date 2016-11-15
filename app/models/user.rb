e2b75d0c7c75187d273e3fe3925fae6062945b0a
Kyle Jobbins <kyle@jobbins.net>
2016-11-15 10:37:58 +0100

    devise update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
- devise :database_authenticatable, :registerable,
- :recoverable, :rememberable, :trackable, :validatable
  has_many :groups, through: :memberships
  has_many :memebership_requests
  has_many :comments, dependent: :destroy
  validates :first_name, presence: true
  validates :last_name, presence: true
  validates :location, presence: true
- validates :photo, presence: true
+ # validates :photo, presence: true
  validates :email, presence: true, uniqueness: true
  validates :mobile, presence: true, uniqueness: true
  # mount_uploader :photo, PhotoUploader
