b2f4cb36e8a79873d36cd30908cffcc92ab66260
Dana Mol <dana.mol6@gmail.com>
2016-11-16 16:35:59 +0100

    Start link requirements

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,7 +9,7 @@ class Group < ApplicationRecord
   validates :description, presence: true
   validates :location, presence: true
   validates :photo, presence: true
-  validates :link, presence: true, uniqueness: true
+  validates :link, presence: true, uniqueness: true, length: { minimum: 22, maximum: 22 }
   validates :capacity, presence: true
   validates :question, presence: :true
   validates :category, inclusion: { in: CATEGORIES, allow_nil: false }, presence: true
