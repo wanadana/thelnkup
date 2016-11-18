819d1ec1b6cefb36b1cdc7dc7a8efd1c49cc925a
Dana Mol <dana.mol6@gmail.com>
2016-11-18 12:09:07 +0100

    Add scopes to memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,5 +3,7 @@ class Membership < ApplicationRecord
   belongs_to :group
 
   scope :pending, -> { where(status: 'pending') }
-  scope :admin, -> {where(admin: true)}
+  scope :admin, -> { where(admin: true) }
+  scope :limit_three, -> { limit(3) }
+  scope :oldest_first, -> { order(:created_at) }
 end
