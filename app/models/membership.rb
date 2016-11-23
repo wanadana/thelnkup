64fdcd1136072bce7334d49a9826bd766375826d
Dana Mol <dana.mol6@gmail.com>
2016-11-23 11:38:06 +0100

    Naming things

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -6,7 +6,7 @@ class Membership < ApplicationRecord
 
   scope :pending, -> { where(status: 'pending') }
   scope :approved, -> { where(status: 'approved') }
-  scope :limit_three, -> { limit(1) }
+  scope :limit_one, -> { limit(1) }
   scope :oldest_first, -> { order(:created_at) }
 
   def accept!
